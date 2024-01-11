9d3f7c08bcb817d6f4ddfb505ed0063474600fda
Hasanul Haque Banna <programmerbanna@gmail.com>
2024-01-12 02:43:24 +0600

    after linked staged now working

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  sgsdgs
   const app = await NestFactory.create(AppModule)
   await app.listen(3000)
 }
