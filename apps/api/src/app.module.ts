8aae85b27f09c5cb162f39c06442ac495a0ddff7
Hasanul Haque Banna <programmerbanna@gmail.com>
2024-01-12 17:37:59 +0600

    done

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,26 @@
+import { join } from 'path'
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+
+// custom file imports
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      introspection: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      buildSchemaOptions: {
+        numberScalarMode: 'integer',
+      },
+      fieldResolverEnhancers: ['guards'],
+    }),
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/api/src/models/users/rest/dtos/query.dto.ts
@@ -0,0 +1,10 @@
+import { IsIn, IsOptional } from 'class-validator'
+import { Prisma } from '@prisma/client'
+import { BaseQueryDto } from 'src/common/dtos/common.dto'
+
+export class UserQueryDto extends BaseQueryDto {
+  @IsOptional()
+  @IsIn(Object.values(Prisma.UserScalarFieldEnum))
+  sortBy?: string
+}
+
